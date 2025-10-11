87c2fd16cc9ac3a132856ce793d4ce33adb5eb2c
Arthur-Aillet <arthur.aillet@epitech.eu>
2025-10-11 19:56:54 +0200

    feat: basic start of the depths

--- public/stores/app.js
+++ public/stores/app.js
@@ -2,6 +2,7 @@ export const cursorData = {
   angle: 0,
   animate: true,
   setMode: (mode) => {}, // cursor || door
+  getMode: () => {}, // cursor || door
   lockCursor: () => {},
   unlockCursor: () => {},
 };
